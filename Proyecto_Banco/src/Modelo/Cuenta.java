34ebe02c0a3a6add806b8767da778b543ea2f519
Jer-Son <jersonhenao30@gmail.com>
2020-08-01 17:47:56 -0500

    cambiosjerson

--- Proyecto_Banco/src/Modelo/Cuenta.java
+++ Proyecto_Banco/src/Modelo/Cuenta.java
@@ -1,13 +1,9 @@
 
 package Modelo;
 
-<<<<<<< HEAD
+
 public class Cuenta  {
-    private int cuentabalance; 
-=======
-public class Cuenta{
     private double cuentabalance; 
->>>>>>> 3d0bafca4d4cf2c5b0cc6255c0028e5d998fcf90
 
     public Cuenta(double cuentabalance) {
         this.cuentabalance = cuentabalance;
